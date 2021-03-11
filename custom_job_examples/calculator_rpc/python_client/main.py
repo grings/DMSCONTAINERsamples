89a84678134414bc207db8d2d92281cced8c1535
Daniele Teti <d.teti@bittime.it>
2021-03-11 10:45:46 +0100

    [+] Added "Diff" call in the python calculator sample

--- custom_job_examples/calculator_rpc/python_client/main.py
+++ custom_job_examples/calculator_rpc/python_client/main.py
@@ -1,6 +1,7 @@
 from calculatorrpc import CalculatorRPCProxy
 proxy = CalculatorRPCProxy("https://localhost/calculatorrpc")
-print(proxy.sum(10,30))
+print("10 + 30 = ", proxy.sum(10,30))
+print("20 - 5 = ", proxy.diff(20,5))
 
 
 
